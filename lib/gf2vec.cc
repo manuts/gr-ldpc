e8b528697bb3d92c4d6764ecf959dac6a77bb402
manuts <manu.t.sree@gmail.com>
2013-09-20 14:42:56 +0530

    Adding copyright

--- lib/gf2vec.cc
+++ lib/gf2vec.cc
@@ -1,3 +1,30 @@
+/*!
+ * \file
+ * \brief Implementation of a class for GF2 vector algebra
+ * \author Manu T S
+ *
+ * -----------------------------------------------------------------
+ *
+ * Copyright 2013 IIT Bombay.
+ * 
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this software; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ *
+ * -----------------------------------------------------------------
+ */
+
 #include "gf2vec.h"
 #include <iostream>
 
@@ -13,6 +40,10 @@ void GF2Vec::set_vec(const std::vector<char> in) {
     for ( int i = 0; i < vec.size(); i++ ) {
         vec[i] = in[i];
     }
+}
+
+std::vector<char> GF2Vec::get_vec(){
+    return vec;
 }
 
 int GF2Vec::size() {
