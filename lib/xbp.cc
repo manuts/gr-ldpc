26f2cef7571e78102fe7beb046ac5132d066ab26
manuts <manu.t.sree@gmail.com>
2013-09-20 15:22:44 +0530

    Adding copyright notice

--- lib/xbp.cc
+++ lib/xbp.cc
@@ -1,3 +1,30 @@
+/*!
+ * \file
+ * \brief Implementation of a class for message passing for bsc channel
+ * \author Manu T S
+ *
+ * -----------------------------------------------------------------
+ *
+ * Copyright 2013 IIT Bombay.
+ * 
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this software; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ *
+ * -----------------------------------------------------------------
+ */
+
 #include "xbp.h"
 
 xbp::xbp ( const GF2Mat X, float sgma) {
